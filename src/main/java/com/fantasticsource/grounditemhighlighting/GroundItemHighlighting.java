a9c573506ff8bba917837fed95490b2448507a63
Kyle Koder <silvertallon@gmail.com>
2019-08-23 17:02:11 -0700

    1.12.2.001; that was easy

---
+++ src/main/java/com/fantasticsource/grounditemhighlighting/GroundItemHighlighting.java
@@ -0,0 +1,37 @@
+package com.fantasticsource.grounditemhighlighting;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = GroundItemHighlighting.MODID, name = GroundItemHighlighting.NAME, version = GroundItemHighlighting.VERSION)
+public class GroundItemHighlighting
+{
+    public static final String MODID = "grounditemhighlighting";
+    public static final String NAME = "Ground Item Highlighting";
+    public static final String VERSION = "1.12.2.001";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(GroundItemHighlighting.class);
+
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+        {
+            //Physical client
+            MinecraftForge.EVENT_BUS.register(ItemHighlighter.class);
+        }
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}

8c8574f5529cb7cf832c16b668afa8b32b9dbef7
Kyle Koder <silvertallon@gmail.com>
2020-01-30 12:28:52 -0800

    Greatly simplified *AND* fixed all bugs afaik

--- src/main/java/com/fantasticsource/grounditemhighlighting/GroundItemHighlighter.java
+++ src/main/java/com/fantasticsource/grounditemhighlighting/GroundItemHighlighter.java
@@ -3,25 +3,20 @@ package com.fantasticsource.grounditemhighlighting;
 import com.fantasticsource.mctools.ClientTickTimer;
 import com.fantasticsource.mctools.items.ItemFilter;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 @SideOnly(Side.CLIENT)
 public class GroundItemHighlighter
 {
     public static ArrayList<ItemFilter> filters = new ArrayList<>();
-    public static HashSet<EntityItem> groundItems = new HashSet<>(), filteredGroundItems = new HashSet<>();
 
     public static void sync()
     {
@@ -30,51 +25,6 @@ public class GroundItemHighlighter
         {
             ItemFilter filter = ItemFilter.getInstance(filterString);
             if (filter != null) filters.add(filter);
-        }
-
-        filteredGroundItems.clear();
-        for (EntityItem item : groundItems) onItemJoin(item);
-    }
-
-    @SubscribeEvent
-    public static void joinWorld(EntityJoinWorldEvent event)
-    {
-        if (!event.getWorld().isRemote) return;
-
-        Entity entity = event.getEntity();
-        if (!(entity instanceof EntityItem)) return;
-
-        EntityItem item = (EntityItem) entity;
-        groundItems.add(item);
-
-        onItemJoin(item);
-    }
-
-    protected static void onItemJoin(EntityItem item)
-    {
-        if (item.getItem().getItem() == Items.AIR)
-        {
-            ClientTickTimer.schedule(1, () -> onItemJoin(item));
-            return;
-        }
-
-        boolean matchesFilter = false;
-        for (ItemFilter filter : filters)
-        {
-            if (filter.matches(item.getItem()))
-            {
-                matchesFilter = true;
-                break;
-            }
-        }
-
-        if (GroundItemHighlightingConfig.whitelist)
-        {
-            if (matchesFilter) filteredGroundItems.add(item);
-        }
-        else
-        {
-            if (!matchesFilter) filteredGroundItems.add(item);
         }
     }
 
@@ -85,25 +35,12 @@ public class GroundItemHighlighter
 
         World world = Minecraft.getMinecraft().world;
 
-        if (world == null)
+        if (world == null) return;
+
+
+        for (EntityItem item : world.getEntities(EntityItem.class, o -> true))
         {
-            groundItems.clear();
-            filteredGroundItems.clear();
-            return;
-        }
-
-
-        for (EntityItem item : groundItems.toArray(new EntityItem[0]))
-        {
-            if (!world.loadedEntityList.contains(item) || item.world != world)
-            {
-                item.setGlowing(false);
-                groundItems.remove(item);
-                filteredGroundItems.remove(item);
-                continue;
-            }
-
-            if (filteredGroundItems.contains(item))
+            if (matchesFilters(item) == GroundItemHighlightingConfig.whitelist)
             {
                 item.setGlowing(GroundItemHighlightingConfig.glow);
 
@@ -114,5 +51,15 @@ public class GroundItemHighlighter
             }
             else item.setGlowing(false);
         }
+    }
+
+    protected static boolean matchesFilters(EntityItem item)
+    {
+        for (ItemFilter filter : filters)
+        {
+            if (filter.matches(item.getItem())) return true;
+        }
+
+        return false;
     }
 }

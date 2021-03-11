07d8eb088bbcc426ed19b64ebfe50f4c84e535ea
Kyle Koder <silvertallon@gmail.com>
2021-03-10 23:18:49 -0800

    1.12.2.003b

--- src/main/java/com/fantasticsource/grounditemhighlighting/GroundItemHighlighting.java
+++ src/main/java/com/fantasticsource/grounditemhighlighting/GroundItemHighlighting.java
@@ -17,7 +17,7 @@ public class GroundItemHighlighting
 {
     public static final String MODID = "grounditemhighlighting";
     public static final String NAME = "Ground Item Highlighting";
-    public static final String VERSION = "1.12.2.003a";
+    public static final String VERSION = "1.12.2.003b";
 
     public static boolean compatTiamatItems = false;
 
